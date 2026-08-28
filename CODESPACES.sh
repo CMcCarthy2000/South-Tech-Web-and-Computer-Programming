0bfb09371fae4afd767813520eacf406469f648f
Conor McCarthy <168587753+CMcCarthy2000@users.noreply.github.com>
2026-08-28 17:14:48 +0000

    Synced from main desktop at South Tech

---
+++ CODESPACES.sh
@@ -0,0 +1,11 @@
+# ======================================================
+# This script syncs all updates to github 
+# Created 8/26/2026
+# ======================================================
+
+
+git add .
+
+git commit -m "Synced from remote codespaces"
+
+git push

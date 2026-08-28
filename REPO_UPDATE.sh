99cade179ee4ff8993ff97e14f844ad38e9fde94
CMCCARTHY2000 <conor@chivanet.org>
2026-08-28 09:53:41 -0500

    Synced from main desktop at South Tech

---
+++ REPO_UPDATE.sh
@@ -0,0 +1,10 @@
+# ======================================================
+
+# Shell script that checks every file in this repo
+# and pushes it to the main repository located on github
+# Created 8/26/2026
+# ======================================================
+
+git add .
+
+git commit -m "Synced from main desktop at South Tech"
